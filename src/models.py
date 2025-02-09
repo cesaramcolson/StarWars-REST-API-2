b8c6abbc0dad2e5d1d5e4b95e88a79dc547f8491
Cesar Amaya Colella <cesar.rafael.gep@gmail.com>
2025-02-09 15:33:20 +0000

    class user, planet and character created

--- src/models.py
+++ src/models.py
@@ -1,19 +1,81 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password_hash = db.Column(db.String(80), nullable=False)
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.set_password(password)
+        db.session.add(self)
+        try:
+            db.session.commit
+        except Exception as error:
+            db.session.rollback()
+            raise Exception(error.args)
 
     def __repr__(self):
         return '<User %r>' % self.username
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
             "email": self.email,
-            # do not serialize the password, its a security breach
+        }
+    
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    description = db.Column(db.String(250))
+
+    def __init__(self, name, description=None):
+        self.name = name
+        self.description = description
+        db.session.add(self)
+        try:
+            db.session.commit
+        except Exception as error:
+            db.session.rollback()
+            raise Exception (error.args)
+        
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description
+        }
+    
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    description = db.Column(db.String(250))
+
+    def __init__(self, name, description=None):
+        self.name = name
+        self.description = description
+        db.session.add(self)
+        try:
+            db.session.commit
+        except Exception as error:
+            db.session.rollback()
+            raise Exception (error.args)
+        
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description
         }
